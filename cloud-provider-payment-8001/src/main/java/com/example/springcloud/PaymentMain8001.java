574825156751d38bfdc413e43a894cd1551e965a
Amdahl <709688530@qq.com>
2021-07-15 07:28:40 +0800

    Initial commit

---
+++ cloud-provider-payment-8001/src/main/java/com/example/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * FileName:com.example.springcloud
+ * Date:2021-07-14 21:29
+ * History
+ * @author 70968
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
